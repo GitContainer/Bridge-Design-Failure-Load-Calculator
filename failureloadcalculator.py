ab7f6c19df4535bcbeffc1c5ba3069ed1cc2afcf
Smile Peng <smilepengg@gmail.com>
2019-01-01 22:50:02 -0500

    less comments version of failureloadcalculator.py

--- failureloadcalculator.py
+++ failureloadcalculator.py
@@ -57,19 +57,12 @@ class failure:
       #check and convert the user inputed value into a float
       n=try_except(n,"Error the number of sections you have entered is not a number","Enter the number of sections in the cross section:")
       while cnt<n:
-         #need the value of the base for area calculations
          b=input("Enter the dimension of the base of the section:")
-         #error check if user input is valid
-         b=try_except(b,"Error, the base you have entered is not a number","Enter the dimension of the base of the section:")
-         #need the value of the height for area calculations as well       
+         b=try_except(b,"Error, the base you have entered is not a number","Enter the dimension of the base of the section:")       
          h=input("Enter the dimension of the height of the section:")
-         #error check if user input is valid
-         h=try_except(h,"Error, the height you have entered is not a number","Enter the dimension of the height of the section:")
-         #need the value of the respective centroidal axis of each section         
+         h=try_except(h,"Error, the height you have entered is not a number","Enter the dimension of the height of the section:")  
          ybar=input("Enter the distance from the centroidal axis of the section to the bottom of the cross section:")
-         #error check if user input is valid
-         ybar=try_except(ybar,"Error, the distance from the centroidal axis of the section to the bottom of the cross section you have entered is not a number", "Enter the distance from the centroidal axis of the section to the bottom of the cross section:")
-         #calculation of area for the respective section        
+         ybar=try_except(ybar,"Error, the distance from the centroidal axis of the section to the bottom of the cross section you have entered is not a number", "Enter the distance from the centroidal axis of the section to the bottom of the cross section:")       
          area=b*h
          #summing numerator
          accum=accum+(area*ybar)
@@ -77,9 +70,7 @@ class failure:
          area_sum=area_sum+area
          #increase counter for next section in the cross section
          cnt=cnt+1
-      #need the value of the total height of the cross section to determine the centroidal axis from the top and from the bottom
       height=input("Enter the total height of the cross section:")
-      #error check if user input is valid
       height=try_except(height,"Error, the total height you have entered is not a number", "Enter the total height of the cross section:")
       #computation of centroidal axis from the bottom and then storing it in the constant that was initialized to 0
       self.ybot=accum/area_sum
@@ -97,21 +88,14 @@ class failure:
       cnt=0
       #user will need to specify how many sections are present in the cross sections
       n=input("Enter the number of sections in the cross section:")
-      #error check if user input is valid
       n=try_except(n,"Error the number of sections you have entered is not a number","Enter the number of sections in the cross section:")
       while cnt<n:
-         #need the value of the base
          b=input("Enter the dimension of the base of the section:")
-         #error check if user input is valid
          b=try_except(b,"Error, the base you have entered is not a number","Enter the dimension of the base of the section:")
-         #need the value of the height
          h=input("Enter the dimension of the height of the section:")
-         #error check if user input is valid
          h=try_except(h,"Error, the height you have entered is not a number","Enter the dimension of the height of the section:")
          ybar=input("Enter the distance from the centroidal axis of the section to the bottom of the cross section:")
-         #error check if user input is valid
          ybar=try_except(ybar,"Error, the distance from the centroidal axis of the section to the bottom of the cross section you have entered is not a number", "Enter the distance from the centroidal axis of the section to the bottom of the cross section:") 
-         #computation of area of section
          area=b*h
          #computation of the first part of the Parallel Axis Theorem
          first=(b*cube(h))/12
@@ -135,22 +119,14 @@ class failure:
       cnt=0
       #user will need to specify how many sections are present in the cross sections
       n=input("Enter the number of sections in the cross section:")
-      #error check if user input is valid
       n=try_except(n,"Error the number of sections you have entered is not a number","Enter the number of sections in the cross section:")
       while cnt<n:
-         #need the value of the base for area calculations
          b=input("Enter the dimension of the base of the section:")
-         #error check if user input is valid
          b=try_except(b,"Error, the base you have entered is not a number","Enter the dimension of the base of the section:")
-         #need the value of the height for area calculations
          h=input("Enter the dimension of the height of the section:")
-         #error check if user input is valid
          h=try_except(h,"Error, the height you have entered is not a number","Enter the dimension of the height of the section:") 
-         #need the distance between centroidal axes for first moment of area calculations 
          distance=input("Enter the distance from the centroidal axis of the section to the centroidal axis:")
-         #error check if user input is valid 
          distance=try_except(distance, "Error, the distance you have entered is not a number", "Enter the distance from the centroidal axis of the section to the centroidal axis:")
-         #computation of area of section
          area=b*h
          #computation of the First Moment of Inertia
          Q=area*distance
@@ -173,19 +149,12 @@ class failure:
       #error check if user input is valid
       n=try_except(n,"Error the number of sections you have entered is not a number","Enter the number of sections in the cross section:")
       while cnt<n:
-         #need the value of the base for area calculations
          b=input("Enter the dimension of the base of the section:")
-         #error check if user input is valid
          b=try_except(b,"Error, the base you have entered is not a number","Enter the dimension of the base of the section:")
-         #need the value of the height for area calculations
          h=input("Enter the dimension of the height of the section:")
-         #error check if user input is valid
          h=try_except(h,"Error, the height you have entered is not a number","Enter the dimension of the height of the section:")
-         #need the distance between centroidal axes for first moment of area calculations 
          distance=input("Enter the distance from the centroidal axis of the section to the centroidal axis:")
-         #error check if user input is valid 
          distance=try_except(distance, "Error, the distance you have entered is not a number", "Enter the distance from the centroidal axis of the section to the centroidal axis:")
-         #computation of area of section
          area=b*h
          #computation of the First Moment of Inertia
          Q=area*distance
@@ -201,11 +170,9 @@ class failure:
 #crushing failure: To determine the crushing failure load, one must use Navier's equation (stress=(moment*centroidal axis)/second moment of inertia). Since moment will be in terms of failure load, one must rearrange for moment. Moment is read from the Bending Moment diagram.
    def crush(self):
       print("You are now calculating for the crushing failure load")
-      #getting the value of the moment
       moment=input("Enter the Moment in terms of failure load:")
-      #error check if user input is valid
       moment=try_except(moment,"Error, the Moment you have entered is not a number","Enter the Moment in terms of failure load:")
-      #one must 
+      #one must indicate if they are calculating using the centroidal axis from the top or from the bottom (depends on Bending Moment diagram)
       y=input("Enter if calculating using ytop or ybot:")
       #error check if user input is valid
       while y!="ytop" and y!="ybot":
@@ -223,9 +190,7 @@ class failure:
 #tension failure: This process is identically to crushing failure except the value of the stress used is different (30MPa instead of 6MPa).
    def tensile(self):
       print("You are now calculating for the tensile failure load")
-      #getting the value of the moment
       moment=input("Enter the Moment in terms of failure load:")
-      #error check if user input is valid
       moment=try_except(moment,"Error, the Moment you have entered is not a number", "Enter the Moment in terms of failure load:")
       y=input("Enter if calculating using ytop or ybot:")
       #error check if user input is valid
@@ -244,13 +209,10 @@ class failure:
 #Matboard shear failure: To determine the Matboard shear failure load, one must use Jourawski's equation (Force=(Shear force*First Moment of Inertia)/Second Moment of Inertia*base). Since shear force  will be in terms of failure load, one must rearrange for the shear force. Shear force is read from the Shear Force diagram at the maximum point.
    def shear(self):
       print("You are now calculating for the matboard shear failure load")
-      #need the value of the base to use Jourawski's equation
       base=input("Enter the dimension of the base:")
-      #error check if user input is valid
       base=try_except(base,"Error, the base you have entered is not a number","Enter the dimension of the base:")
       #need the value of the Shear Force according to the Shear Force diagram to use Jourawski's equation
       V=input("Enter the Shear Force in terms of failure load:")
-      #error check if user input is valid
       V=try_except(V,"Error, the Shear Force you have entered is not a number", "Enter the Shear Force in terms of failure load:")
       #computation of matboard shear failure load
       p=(self.I*base*self.T_matboard)/(self.Qshear*V)
@@ -260,13 +222,10 @@ class failure:
 #Glue shear failure:This process is identically to crushing failure except the First Moment of Inertia is different for the glue (use Qglue instead of Qshear)
    def glue_shear(self):
       print("You are now calculating for the glue shear failure load")
-      #need the value of the base to use Jourawski's equation
       base=input("Enter dimension of the base:")
-      #error check if user input is valid
       base=try_except(base,"Error, the base you have entered is not a number","Enter the dimension of the base:")
       #need the value of the Shear Force according to the Shear Force diagram to use Jourawski's equation
       V=input("Enter the Shear Force in terms of failure load:")
-      #error check if user input is valid
       V=try_except(V,"Error, the Shear Force you have entered is not a number", "Enter the Shear Force in terms of failure load:")
       #computation of glue shear failure load
       p=(self.I*base*self.T_glue)/(self.Qglue*V)
@@ -277,20 +236,13 @@ class failure:
 #2 fixed ends: To determine the plate buckling failure load with 2 fixed ends, one must use the plate buckling equation [((4*pi^2*young's modulus)/(12*(1-poisson's variable^2)))*(t/b)^2, where t is the thickness and b is the base] and compare it to the stress calculated by using Navier's equation. 
    def two_fix(self):
       print("You are now calculating for the thin plate buckling failure load with 2 fixed ends")
-      #need the thickness to use plate buckling equation
       t=input("Enter the dimension of the thickness:")
-      #error check if user input is valid
       t=try_except(t,"Error, the thickness you have entered is not a number", "Enter the dimension of the thickness:")
-      #need the base to use plate buckling equation
       b=input("Enter dimension of the base:")
-      #error check if user input is valid
       b=try_except(b,"Error, the base you have entered is not a number", "Enter the dimension of the base:")
-      #getting the value of the moment based on the Bending Moment diagram at the appropriate point to use in the plate buckling equation
       moment=input("Enter the Moment in terms of failure load:")
-      #error check if user input is valid
       moment=try_except(moment,"Error, the Moment you have entered is not a number", "Enter the Moment in terms of failure load:")
-      y=input("Enter if calculating using ytop or ybot:")
-      #error check if user input is valid
+      y=input("Enter if calculating using ytop or ybot:")      #error check if user input is valid
       while y!="ytop" and y!="ybot":
          print("Error, did not enter ytop or ybot")
          y=input("Enter if calculating using ytop or ybot:")
@@ -309,20 +261,13 @@ class failure:
 #1 free edge:This process is identically to the 2 fixed end calculations except different dimension values (ex: base value) are used and the plate buckling equation is slightly different, multiplying by 0.425 instead of 4. 
    def free_edge(self):
       print("You are now calculating for the thin plate buckling failure load with 1 free edge")
-      #need the thickness to use plate buckling equation
       t=input("Enter the dimension of the thickness:")
-      #error check if user input is valid
       t=try_except(t,"Error, the thickness you have entered is not a number", "Enter the dimension of the thickness:")
-      #need the thickness to use plate buckling equation
       b=input("Enter dimension of the base:")
-      #error check if user input is valid
       b=try_except(b,"Error, the base you have entered is not a number", "Enter the dimension of the base:")
-      #getting the value of the moment based on the Bending Moment diagram at the appropriate point to use in the plate buckling equation
       moment=input("Enter the Moment in terms of failure load:")
-      #error check if user input is valid
       moment=try_except(moment,"Error, the Moment you have entered is not a number", "Enter the Moment in terms of failure load:")
       y=input("Enter if calculating using ytop or ybot:")
-      #error check if user input is valid
       while y!="ytop" and y!="ybot":
          print("Error, did not enter ytop or ybot")
          y=input("Enter if calculating using ytop or ybot:")
@@ -342,20 +287,13 @@ class failure:
 #web buckling: Again, this process is identically to the 2 fixed end calculations except different dimension values (ex: base value) are used and the plate buckling equation is slightly different, multiplying by 6 instead of 4. 
    def web_buckling(self):
       print("You are now calculating for the web buckling failure load")
-      #need the thickness to use plate buckling equation
       t=input("Enter the dimension of the thickness:")
-      #error check if user input is valid
       t=try_except(t,"Error, the thickness you have entered is not a number", "Enter the dimension of the thickness:")
-      #need the thickness to use plate buckling equation
       b=input("Enter dimension of the base:")
-      #error check if user input is valid
       b=try_except(b,"Error, the base you have entered is not a number", "Enter the dimension of the base:")
-      #getting the value of the moment based on the Bending Moment diagram at the appropriate point to use in the plate buckling equation
       moment=input("Enter the Moment in terms of failure load:")
-      #error check if user input is valid
       moment=try_except(moment,"Error, the Moment you have entered is not a number", "Enter the Moment in terms of failure load:")
       y=input("Enter if calculating using ytop or ybot:")
-      #error check if user input is valid
       while y!="ytop" and y!="ybot":
          print("Error, did not enter ytop or ybot")
          y=input("Enter if calculating using ytop or ybot:")
@@ -375,24 +313,15 @@ class failure:
 #diaphragm shear: Once again, this process is very similar to the 2 fixed end calculations except different dimension values (ex: base value) are used, additional dimension values are needed such as the spacing between each diaphragm, and the plate buckling equation is different. The plate buckling equation used to find the critical Shear Force is instead ((5*pi^2*Young's Modulus)/(12*(1-poisson's variable^2)))*((t/a)^2+(t/h)^2), where a is the spacing between diaphragms. 
    def diashear(self):
       print("You are now calculating for the diaphragm shear failure load")
-      #need the thickness to use diaphragm shear equation
       t=input("Enter the dimension of the thickness:")
-      #error check if user input is valid
       t=try_except(t,"Error, the thickness you have entered is not a number", "Enter the dimension of the thickness:")
-      #need the thickness to use diaphragm shear equation
       b=input("Enter dimension of the base:")
-      #error check if user input is valid
       b=try_except(b,"Error, the base you have entered is not a number", "Enter the dimension of the base:")
-      #need the height to use diaphragm shear equation
       h=input("Enter the height:")
-      #error check if user input is valid
       h=try_except(h,"Error, the height you have entered is not a number", "Enter the height:")
-      #need the spacing between diaphragms to use diaphragm shear equation
       a=input("Enter the spacing between diaphragms:")
-      #error check if user input is valid
       a=try_except(a,"Error, the spacing you have entered is not a number","Enter the spacing between diaphragms:")
       V=input("Enter the Shear Force in terms of failure load:")
-      #error check if user input is valid
       V=try_except(V,"Error, the Shear Force you have entered is not a number", "Enter the Shear Force in terms of failure load:")
       Tcrit=((5*square(3.141592653589793)*self.young_modulus)/(12*(1-square(self.poisson))))*(square(t/a)+square(t/h))
       p=(Tcrit*self.I*b)/(self.Qshear*V)
